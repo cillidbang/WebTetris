25121c982e9b85dd68eb0c73d985f3f033217bec
Dorian <dorian882004@gmail.com>
2026-04-08 22:17:16 +0200

    correctly placed on board, but not in last positions.

--- js/board.js
+++ js/board.js
@@ -9,32 +9,32 @@ export class Board {
     placed = 'O';
     empty = '.';
     pending = 'X';
-    
-    placedPositions = [];
+
 
     constructor() {
         this.clearBoardSavePlaced();
     }
     clearBoardSavePlaced() {
-        this.placedPositions = [];
+        const placedPositions = [];
         
         for (let row = 0; row <= this.board.length - 1; row++) {
             for (let column = 0; column <= this.board[row].length - 1; column++) {
                 if (this.board[row][column] === this.pending) {
-                    this.placedPositions.push({x: row, y: column});
+                    placedPositions.push({x: row, y: column});
                 }
                 if (this.board[row][column] !== this.placed) {
                     this.board[row][column] = this.empty;
                 }
             }
         }
-        return this.placedPositions;
+        return placedPositions;
     }
 
     async figureFallUntilCollision(insertColumn, figureArray) {
         const lastIndexForPlacement = (this.board.length - 1) - (figureArray.length - 1);
         let lastPlacedPositions = [];
         for (let row = 0; row <= lastIndexForPlacement; row++) {
+
             const isLastRow = row === lastIndexForPlacement;
             const fieldStatus = isLastRow ? this.placed : this.pending;
 
@@ -42,36 +42,32 @@ export class Board {
                 this.restoreLastPlacedPositions(lastPlacedPositions);
                 
                 window.dispatchEvent(new CustomEvent('color-it', {
-                    detail: this.placedPositions,
+                    detail: lastPlacedPositions,
                     bubbles: true,
                     composed: true,
-                }))
+                }));
                 return;
             }
             this.insertFigureAtCoordinates(row, insertColumn, fieldStatus, figureArray);
-            
-            
-            lastPlacedPositions = this.clearBoardSavePlaced();
-
             window.dispatchEvent(new CustomEvent('color-it', {
-                detail: this.placedPositions,
+                detail: lastPlacedPositions,
                 bubbles: true,
                 composed: true,
-            }))
-            
+            }));
+            lastPlacedPositions = this.clearBoardSavePlaced();
         }
     }
 
-    restoreLastPlacedPositions() {
-        for (let position of this.placedPositions) {
+    restoreLastPlacedPositions(lastPlacedPositions) {
+        for (let position of lastPlacedPositions) {
             this.board[position.x][position.y] = this.placed;
         }
     }
 
     nextPlacementWillCollide(lastPositions) {
         for (let position of lastPositions) {
-            const outOfBounds = position.x < this.board.length - 1 && position.y < this.board[position.x].length - 1;
-            if (outOfBounds) {
+            const innerBonds = position.x < this.board.length - 1 && position.y < this.board[position.x].length - 1;
+            if (innerBonds) {
                 if (this.board[position.x + 1][position.y] === this.placed) {
                     return true;
                 }
